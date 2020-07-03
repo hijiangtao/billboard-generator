dc21635e85e6bd1a4eed30c6feca1440f38361e8
Tao Jiang <hijiangtao@gmail.com>
2020-07-03 19:14:36 +0800

    ADD: Configure project for Circle CI

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/canvas-billboard'),
+      dir: require('path').join(__dirname, './coverage/billboard-generator'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
