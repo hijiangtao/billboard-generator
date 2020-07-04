219112ea03e0eb4d97d25e5f81efeb7908013b2c
Tao Jiang <hijiangtao@gmail.com>
2020-07-04 16:53:35 +0800

    MOD: implement e2e test demo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display logo text', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('billboard-generator app is running!');
+    expect(page.getTitleText()).toEqual('Billboard Generator');
   });
 
   afterEach(async () => {
