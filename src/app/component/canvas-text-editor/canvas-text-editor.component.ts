ca281a7a512769048d2472d4b5706217b3131e6a
Tao Jiang <hijiangtao@gmail.com>
2020-08-16 22:06:28 +0800

    MOD: refactor text editor logic

--- src/app/component/canvas-text-editor/canvas-text-editor.component.ts
+++ src/app/component/canvas-text-editor/canvas-text-editor.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 declare const MediaRecorder: any;
 
@@ -12,8 +12,11 @@ enum TextType {
 const OPTIONS = {
   x: 10,
   y: 10,
+  width: 2000,
+  height: 900,
   fontSize: 100,
-  lineHeight: 140,
+  lineHeight: 120,
+  selectionBgColor: 'rgba(0,0,0,0.5)',
 };
 
 @Component({
@@ -21,7 +24,7 @@ const OPTIONS = {
   templateUrl: './canvas-text-editor.component.html',
   styleUrls: ['./canvas-text-editor.component.scss'],
 })
-export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
+export class CanvasTextEditorComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('textEditor')
   textEditor: ElementRef;
 
@@ -33,21 +36,33 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
 
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
+  textElement: HTMLTextAreaElement;
+  recorder: any;
 
-  textInputType: TextType;
-
+  scaleRatio = 1;
+  textInputType: TextType = TextType.ENGLISH;
   textList: string[] = ['这是一段初始化文案', '就这样吧'];
   cursorPos: number[] = [0, 0];
+
+  /**
+   * Command 键是否被按下
+   */
   isCommandKey: boolean;
-  selected: any;
-  localArr: any = [];
 
-  videoUrl: SafeUrl;
+  /**
+   * 是否聚焦文本编辑器
+   */
+  isTextSelected: boolean;
 
-  raf: any;
+  /**
+   * 文本是否全选
+   */
+  isSelectAll = false;
 
-  // recordVideo$: ReplaySubject<boolean> = new ReplaySubject(1);
-  recorder: any;
+  cursorTextList: any = [];
+
+  videoUrl$: ReplaySubject<SafeUrl> = new ReplaySubject(1);
+  recordVideo$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(private sanitizer: DomSanitizer) {}
 
@@ -56,171 +71,127 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.canvas = this.textEditor.nativeElement;
     this.context = this.canvas.getContext('2d');
+    this.textElement = this.textArea.nativeElement;
+    this.scaleRatio = this.canvas.clientWidth / this.canvas.width;
 
-    this.initTextEditor();
-    this.initTextInputListener();
-    this.renderEditorContent(true);
+    this.initTextEditorLogic();
+    this.drawTextEditorLogic(true);
   }
 
-  initTextEditor() {
-    this.canvas.addEventListener('click', this.focusOnEditor.bind(this));
+  initTextEditorLogic() {
+    this.canvas.addEventListener('click', this.focusEditorHandler.bind(this));
+    document.addEventListener('click', this.blurEditorHandler.bind(this));
 
-    document.addEventListener('click', (e) => {
-      if (e.target !== this.canvas) {
-        this.canvas.style.borderColor = 'gray';
-      }
-    });
+    this.textElement.addEventListener('compositionstart', this.chineseTextInputHandler, false);
+    this.textElement.addEventListener('input', this.normalTextInputHandler, false);
+    this.textElement.addEventListener('compositionend', this.commonTextInputEndHandler, false);
+
+    this.textElement.addEventListener('keydown', this.onKeyDownHandler.bind(this));
+    this.textElement.addEventListener('keyup', this.onKeyUpHandler.bind(this));
   }
 
-  calculateCursorPos(x, y) {
-    let isMoved = false;
-    let xLabel = 0;
-    let yLabel = 0;
+  /**
+   * Event Handlers
+   */
 
-    // 计算垂直位于哪一行
-    for (let i = 0; i < this.localArr.length; i++) {
-      const start = this.localArr[i][0].y.start;
-      const end = this.localArr[i][0].y.end;
-      if (start < y && y < end) {
-        yLabel = i;
-      }
+  chineseTextInputHandler = () => {
+    this.textInputType = TextType.CHINESE;
+  };
+
+  normalTextInputHandler = (e) => {
+    if (this.textInputType === TextType.CHINESE) {
+      return;
     }
-    // 计算水平位于哪一列
-    for (let j = 0; j < this.localArr[yLabel]?.length; j++) {
-      // 计算水平位于哪一个位置，即与所有点相比较，最近的也就是绝对距离较为小的那个点
-      const abs = Math.abs(this.localArr[yLabel][j].x - x);
-      if (abs <= OPTIONS.fontSize / 2) {
-        isMoved = true;
-        xLabel = j;
-        break;
-      }
-    }
-    return { isMoved, xLabel, yLabel };
-  }
 
-  focusOnEditor(e) {
+    // 处理英文输入
+    this.updateTextList(e.data);
+    this.drawTextEditorLogic(true);
+  };
+
+  commonTextInputEndHandler = (e) => {
+    if (this.textInputType === TextType.CHINESE) {
+      this.updateTextList(e.data);
+      this.drawTextEditorLogic(true);
+      this.textInputType = TextType.ENGLISH;
+    }
+  };
+
+  focusEditorHandler(e) {
     this.canvas.style.borderColor = '#f00';
-    const scale = this.canvas.clientWidth / this.canvas.width;
-    const clientX = e.offsetX / scale;
-    const clientY = e.offsetY / scale;
-    // console.log(clientX, clientY, e, scale, this.canvas);
-    const { xLabel, yLabel } = this.calculateCursorPos(clientX, clientY);
 
-    this.textArea.nativeElement.focus();
+    const clientX = e.offsetX / this.scaleRatio;
+    const clientY = e.offsetY / this.scaleRatio;
+    const { colIndex, rowIndex } = this.calculateCursorPos(clientX, clientY);
 
-    this.cursorPos = [yLabel, xLabel];
-
-    this.renderEditorContent(true);
+    this.textElement.focus();
+    this.cursorPos = [rowIndex, colIndex];
+    this.isTextSelected = true;
+    this.drawTextEditorLogic(true);
   }
 
-  updateTextList(data) {
-    const [startIndex, endIndex] = this.cursorPos;
-    const arr = this.textList[startIndex]?.split('') || [];
-    arr.splice(endIndex, 0, data);
-    this.textList[startIndex] = arr.join('');
-    const strLength = data?.length || 0;
-    this.cursorPos[1] = endIndex + strLength;
+  blurEditorHandler(e) {
+    if (e.target !== this.canvas) {
+      this.canvas.style.borderColor = 'gray';
+      this.isTextSelected = false;
+    }
+    this.isSelectAll = false;
+    this.drawTextEditorLogic(true);
   }
 
-  initTextInputListener() {
-    this.textArea.nativeElement.addEventListener(
-      'compositionstart',
-      (e) => {
-        // console.log(e.data);
-        this.textInputType = TextType.CHINESE;
-      },
-      false
-    );
-    this.textArea.nativeElement.addEventListener(
-      'input',
-      (e) => {
-        // console.log(e.data);
-
-        if (this.textInputType === TextType.CHINESE) {
-          return;
-        }
-        // 处理英文输入
-        this.updateTextList(e.data);
-
-        this.textInputType = TextType.ENGLISH;
-        this.renderEditorContent(true);
-      },
-      false
-    );
-
-    // 中文输入
-    this.textArea.nativeElement.addEventListener(
-      'compositionend',
-      (e) => {
-        if (this.textInputType === TextType.CHINESE) {
-          this.updateTextList(e.data);
-          this.renderEditorContent(true);
-          this.textInputType = TextType.ENGLISH;
-        }
-      },
-      false
-    );
-
-    this.textArea.nativeElement.addEventListener('keydown', this.onKeyDown.bind(this));
-    this.textArea.nativeElement.addEventListener('keyup', this.onKeyUp.bind(this));
-  }
-
-  onKeyUp() {
+  onKeyUpHandler() {
     this.isCommandKey = false;
-    this.renderEditorContent(true);
+    this.drawTextEditorLogic(true);
   }
 
-  onKeyDown(e) {
+  onKeyDownHandler(e) {
     switch (e.key) {
       case 'Meta':
       case 'Control':
         this.isCommandKey = true;
         break;
       case 'a':
-        if (this.isCommandKey === true) {
-          this.selected = true;
-          this.renderEditorContent(true);
-        }
+        this.onAKeyHandler();
         break;
       case 'Backspace':
-        if (this.selected) {
-          this.cursorPos = [0, 0];
-          this.textList = [];
-          this.selected = false;
-          this.renderEditorContent(true);
-        } else {
-          for (let i = 0; i < this.textList.length; i++) {
-            let str = '';
-            for (let j = 0; j < this.textList[i].length; j++) {
-              if (i === this.cursorPos[0]) {
-                if (j !== this.cursorPos[1] - 1) {
-                  str += this.textList[i][j];
-                }
-              } else {
-                str += this.textList[i][j];
-              }
-            }
-            this.textList[i] = str;
-
-            if (i === this.cursorPos[0]) {
-              this.cursorPos[1]--;
-              // 换行
-              if (this.cursorPos[1] < 0) {
-                if (i !== 0) {
-                  debugger;
-                  // 删除一行
-                  this.textList.splice(this.cursorPos[0], 1);
-                  this.cursorPos = [i - 1, this.textList[i - 1].length];
-                } else {
-                  this.cursorPos[1] = 0;
-                }
-              }
-            }
-            this.renderEditorContent(true);
-          }
-        }
+        this.onBackspaceKeyHandler();
         break;
       case 'ArrowRight':
+        this.onHorizontalArrowKeyHandler('right');
+        break;
+      case 'ArrowLeft':
+        this.onHorizontalArrowKeyHandler('left');
+        break;
+
+      case 'ArrowUp':
+        this.onVerticalArrowKeyHandler('up');
+        break;
+      case 'ArrowDown':
+        this.onVerticalArrowKeyHandler('down');
+        break;
+      case 'Enter':
+        this.onEnterKeyHandler();
+        break;
+    }
+  }
+
+  onAKeyHandler() {
+    if (this.isCommandKey) {
+      this.isSelectAll = true;
+    }
+  }
+
+  onEnterKeyHandler() {
+    const arr = this.textList.concat([]);
+    const index = this.cursorPos[0];
+    arr.splice(index + 1, 0, '');
+    this.textList = arr;
+    this.cursorPos[0]++;
+    this.cursorPos[1] = 0;
+  }
+
+  onHorizontalArrowKeyHandler(direction: 'right' | 'left') {
+    switch (direction) {
+      case 'right':
         this.cursorPos[1] += 1;
         if (this.cursorPos[1] > this.textList[this.cursorPos[0]].length) {
           if (this.cursorPos[0] < this.textList.length - 1) {
@@ -230,9 +201,9 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
             this.cursorPos[1] = this.textList[this.cursorPos[0]].length;
           }
         }
-        this.renderEditorContent(true);
         break;
-      case 'ArrowLeft':
+      case 'left':
+      default:
         this.cursorPos[1] -= 1;
         if (this.cursorPos[1] < 0) {
           if (this.cursorPos[0] > 0) {
@@ -242,11 +213,13 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
             this.cursorPos[1] = 0;
           }
         }
-
-        this.renderEditorContent(true);
         break;
+    }
+  }
 
-      case 'ArrowUp':
+  onVerticalArrowKeyHandler(direction: 'up' | 'down') {
+    switch (direction) {
+      case 'up':
         // 边界判断
         if (this.cursorPos[0] > 0) {
           this.cursorPos[0]--;
@@ -255,7 +228,7 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
           }
         }
         break;
-      case 'ArrowDown':
+      case 'down':
         if (this.cursorPos[0] < this.textList.length - 1) {
           this.cursorPos[0]++;
           if (this.cursorPos[1] > this.textList[this.cursorPos[0]].length) {
@@ -263,75 +236,129 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
           }
         }
         break;
-      case 'Enter':
-        const arr = this.textList.concat([]);
-        const index = this.cursorPos[0];
-        arr.splice(index + 1, 0, '');
-        this.textList = arr;
-        this.cursorPos[0]++;
-        this.cursorPos[1] = 0;
-        this.renderEditorContent(true);
-        break;
     }
   }
 
+  onBackspaceKeyHandler() {
+    if (this.isSelectAll) {
+      this.cursorPos = [0, 0];
+      this.textList = [];
+      this.isSelectAll = false;
+    } else {
+      for (let i = 0; i < this.textList.length; i++) {
+        if (i === this.cursorPos[0]) {
+          const str = this.textList[i].split('');
+          str.splice(this.cursorPos[1] - 1, 1);
+
+          this.textList[i] = str.join('');
+
+          this.cursorPos[1] -= 1;
+          // 换行
+          if (this.cursorPos[1] < 0) {
+            if (i !== 0) {
+              const [delString] = this.textList.splice(this.cursorPos[0], 1);
+              this.cursorPos = [i - 1, this.textList[i - 1].length];
+              this.textList[i - 1] += delString;
+            } else {
+              this.cursorPos[1] = 0;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * 计算光标位置
+   * @param x 水平像素坐标
+   * @param y 垂直像素坐标
+   */
+  calculateCursorPos(x: number, y: number) {
+    let colIndex = 0;
+    let rowIndex = 0;
+
+    // 计算垂直位于哪一行
+    for (let i = 0; i < this.cursorTextList.length; i++) {
+      const { start, end } = this.cursorTextList[i][0].y;
+
+      if (start < y && y < end) {
+        rowIndex = i;
+        break;
+      }
+    }
+
+    // 计算水平位于哪一列
+    for (let j = 0; j < this.cursorTextList[rowIndex]?.length; j++) {
+      const abs = Math.abs(this.cursorTextList[rowIndex][j].x - x);
+      if (abs <= OPTIONS.fontSize / 2) {
+        colIndex = j;
+        break;
+      }
+    }
+    return { colIndex, rowIndex };
+  }
+
+  updateTextList(data: string) {
+    const [startIndex, endIndex] = this.cursorPos;
+    const arr = this.textList[startIndex]?.split('') || [];
+    arr.splice(endIndex, 0, data);
+    this.textList[startIndex] = arr.join('');
+    const strLength = data?.length || 0;
+    this.cursorPos[1] = endIndex + strLength;
+  }
+
   relocateTextList = () => {
+    this.context.save();
+
     this.context.font = `${OPTIONS.fontSize}px Arial`;
     this.context.lineWidth = 1;
 
-    // Relocate
-    this.localArr = [];
+    this.cursorTextList = [];
     this.context.textBaseline = 'bottom';
     for (let i = 0; i < this.textList.length; i++) {
       const str = this.textList[i];
       const arr = this.textList[i].split('');
       const curLocalItem = [];
       for (let j = 0; j < arr.length; j++) {
-        const x = OPTIONS.x + this.canvas.offsetLeft + this.context.measureText(str.slice(0, j)).width;
-        const ystart = OPTIONS.y + this.canvas.offsetTop + OPTIONS.lineHeight * i;
+        const x = OPTIONS.x + this.context.measureText(str.slice(0, j)).width;
+        const ystart = OPTIONS.y + OPTIONS.lineHeight * i;
         const yend = ystart + OPTIONS.lineHeight;
         curLocalItem.push({ x, y: { start: ystart, end: yend } });
       }
-      this.localArr.push(curLocalItem);
+      this.cursorTextList.push(curLocalItem);
     }
+
+    this.context.restore();
   };
 
-  renderEditorContent(raf: boolean = false) {
-    // if (raf) {
-    //   window.cancelAnimationFrame?.(this.raf);
-    //   window.webkitCancelAnimationFrame?.(this.raf);
-    //   this.raf = this.renderEditorContent.bind(this);
-    // }
-
+  drawTextEditorLogic(raf: boolean = false) {
     this.context.fillStyle = '#fff';
-    this.context.fillRect(0, 0, 2000, 900);
+    this.context.fillRect(0, 0, OPTIONS.width, OPTIONS.height);
     this.context.font = `${OPTIONS.fontSize}px Arial`;
-    this.context.fillStyle = '#000';
+    this.context.fillStyle = this.isTextSelected ? '#000' : '#555';
+    this.context.textBaseline = 'middle';
 
     this.relocateTextList();
 
-    // write text
-    // 二维而言,就是一个二维的数组
     for (let i = 0; i < this.textList.length; i++) {
       let str = '';
       for (let j = 0; j < this.textList[i].length; j++) {
-        if (!this.selected && this.cursorPos[0] === i && this.cursorPos[1] === j) {
+        if (this.isTextSelected && !this.isSelectAll && this.cursorPos[0] === i && this.cursorPos[1] === j) {
           str += '|';
         }
         str += this.textList[i][j];
       }
 
       // 选中区域的设置，设置选中区域的背景
-      if (this.selected) {
-        let _width = this.context.measureText(this.textList[i]).width;
-        this.context.fillStyle = 'rgba(0,0,0,0.5)';
-        this.context.fillRect(OPTIONS.x, OPTIONS.y + OPTIONS.lineHeight * i, _width, OPTIONS.lineHeight);
+      if (this.isSelectAll) {
+        const selectionWidth = this.context.measureText(this.textList[i]).width;
+        this.context.fillStyle = OPTIONS.selectionBgColor;
+        this.context.fillRect(OPTIONS.x, OPTIONS.y + OPTIONS.lineHeight * i, selectionWidth, OPTIONS.lineHeight);
       }
-      // 开始绘制文字
-      this.context.fillText(str, OPTIONS.x, OPTIONS.y + OPTIONS.lineHeight * (i + 1));
-    }
 
-    // window.requestAnimationFrame(this.raf.bind(this));
+      // 开始绘制文字
+      this.context.fillText(str, OPTIONS.x, OPTIONS.y + OPTIONS.lineHeight * (i + 0.5));
+    }
   }
 
   startRecording() {
@@ -341,7 +368,6 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
     const data = [];
     this.recorder.ondataavailable = (event) => {
       if (event.data && event.data.size) {
-        console.log(event.data);
         data.push(event.data);
       }
     };
@@ -349,13 +375,29 @@ export class CanvasTextEditorComponent implements OnInit, AfterViewInit {
     this.recorder.onstop = () => {
       const url = URL.createObjectURL(new Blob(data, { type: 'video/webm' }));
 
-      this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+      this.videoUrl$.next(this.sanitizer.bypassSecurityTrustUrl(url));
     };
 
     this.recorder.start();
+    this.recordVideo$.next(true);
   }
 
   stopRecording() {
     this.recorder.stop();
+    this.recordVideo$.next(false);
+  }
+
+  ngOnDestroy() {
+    this.recorder = null;
+
+    this.canvas.removeEventListener('click', this.focusEditorHandler.bind(this));
+    document.removeEventListener('click', this.blurEditorHandler.bind(this));
+
+    this.textElement.removeEventListener('compositionstart', this.chineseTextInputHandler, false);
+    this.textElement.removeEventListener('input', this.normalTextInputHandler, false);
+    this.textElement.removeEventListener('compositionend', this.commonTextInputEndHandler, false);
+
+    this.textElement.removeEventListener('keydown', this.onKeyDownHandler.bind(this));
+    this.textElement.removeEventListener('keyup', this.onKeyUpHandler.bind(this));
   }
 }
